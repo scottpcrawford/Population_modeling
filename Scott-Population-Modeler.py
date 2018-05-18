e063f5acd4cd60dbdcab71a59127d7b366b0a8f3
Scott Crawford <scott@crawhouse.com>
2018-05-17 21:50:15 -0400

    Code cleanup
    Code cleanup and refactoring

--- Scott-Population-Modeler.py
+++ Scott-Population-Modeler.py
@@ -18,7 +18,7 @@ class Creature:
         self.genome = []
         self.genome_length = 10 # change this to make a longer genome
         self.allele_length = 2
-        self.generations_remaining = random.randint(1,4)
+        self.generations_remaining = random.randint(1,4) # creature will be capable of reproduction for 1 - 4 generations
         self.capable_of_breeding = True
 
     def get_older(self):
@@ -26,13 +26,13 @@ class Creature:
         if self.generations_remaining < 1:
             self.capable_of_breeding = False
 
-    def make_sodomite(self):
+    def make_sterile(self):
         self.capable_of_breeding = False
 
 
     # randomly generate a genome sequence for our initial population
     def generate_genome(self):
-        for i in range (a.genome_length):
+        for i in range (self.genome_length):
             self.genome.append(tuple(np.random.randint(2, size=self.allele_length))) # generate a genome with random values for each allele
 
 # =============================================================================
@@ -49,10 +49,13 @@ class Population:
         self.members.append(creature) # add a creature to the population pool
 
     def print_members(self): # helper file to see who is in the group
-        print ("These are the current group members: ")
+        print ("\nCurrent breeding members: ")
         for creature in self.members:
-            print(creature.name,"- sex:",creature.sex,"- Can breed?", creature.capable_of_breeding,"- virility remaining: ",creature.generations_remaining,", genome:",creature.genome)
+            print(creature.name,'genome:',creature.genome)
         print("")
+
+    def population_size(self):
+        return len(self.members)
 
     def return_member_list(self):
         return self.members
@@ -63,63 +66,72 @@ class Population:
     def clean_up(self):
         for item in self.members:
             if item.generations_remaining < 1:
+                #print("removing",item.name,"from breeding population")
                 self.remove_member(item)
 
 # do gross stuff
     def mate(self, parentA, parentB):
         if parentA.capable_of_breeding and parentB.capable_of_breeding:
             if parentA.name == parentB.name:
-                return #don't f- yourself
+                return #no self-fertilization
             if parentA.sex == parentB.sex:
-                print(parentA.name,"shall not lay down with ",parentB.name,",breeding impossible, God will randomly smite one of them")
-                if random.randint(0,100) < 2: # 2% chance of God being an awful human being
-                    random.choice([parentA,parentB]).make_sodomite() # remove one from breeding pool
+                if random.randint(0,100) < 2: # 2% chance of one member deciding not to breed
+                    random.choice([parentA,parentB]).make_sterile() # remove one from breeding pool
             else:
-                playGod = random.choice(names_list) # randomly choose a name and gender from the names_list
-                a = Creature(playGod[0],playGod[1]) # create a new instance of a creature with a name and sex
+                creatureAttribute = random.choice(names_list) # randomly choose a name and gender from the names_list
+                a = Creature(creatureAttribute[0],creatureAttribute[1]) # create a new instance of a creature with a name and sex
                 aSplit, bSplit = [], [] # create empty list for parents chromosomes
                 for item in parentA.genome: # split parent A's chromosomes in half
                     aSplit.append(item[random.randint(0,1)])
                 for item in parentB.genome: # split parent B's chromosomes in half
                     bSplit.append(item[random.randint(0,1)])
                     a.genome = list(zip(aSplit,bSplit)) # combine the chromosomes in their new offspring
-                print(parentA.name," & ",parentB.name,'f-ed each other and now there is a new creature:',a.name)
-            #   print ("aSplit: ",aSplit) # print parentA's chromosome split
-            #   print ("bSplit: ",bSplit) # print parentB's chromosome split
+                #print(parentA.name," & ",parentB.name,'mated to produce:',a.name)
                 self.add_member(a) # add the offspring to the population
 
 # =============================================================================
+def setUpWorld(initialPop):
+    a = Creature('Adam', 'M')
+    b = Creature('Eve', 'F')
+    a.generate_genome()
+    b.generate_genome()
+    myPopulation = Population(initialPop) # create a population
+    myPopulation.add_member(a) # add the initial pairing
+    myPopulation.add_member(b) # and don't forget the ladies
+    for x in range(0,initialPop): # have them do the nasty to build out your first generation.. Cain and Abel and so forth
+        myPopulation.mate(a,b)
+    return a,b,myPopulation
 
-# create our initial population and randomly generate a genome for them
-a = Creature('Adam', 'M')
-b = Creature('Eve', 'F')
-a.generate_genome()
-b.generate_genome()
-initialPopulationSize = int(input('Enter Initial Population Size: '))
-#initialPopulationSize = 50
-numberOfGenerations = int(input('Enter Number of Generations: '))
-#numberOfGenerations = 10
-myPopulation = Population(initialPopulationSize) # create a population
-myPopulation.add_member(a) # add the initial pairing
-myPopulation.add_member(b) # and don't forget the ladies
-for x in range(0,initialPopulationSize): # have them do the nasty to build out your first generation.. Cain and Abel and so forth
-    myPopulation.mate(a,b)
+        
+def processGeneration(currentPop):
+    populationList = currentPop.return_member_list()
+    for creature in populationList:
+        currentPop.mate(creature,random.choice(populationList)) # first shot at love
+        currentPop.mate(creature,random.choice(populationList)) # second shot at love
+        creature.get_older()
+    currentPop.clean_up()
+    print("population size is: ",currentPop.population_size())
+    return currentPop
+        
 
-#print (a.name,": ", a.genome)
-#print (b.name,": ", b.genome)
 
-#myPopulation.print_members()
 
-for i in range(0,numberOfGenerations):
+def main():
+    # create our initial population and randomly generate a genome for them
+    initialPopulationSize = int(input('Enter Initial Population Size: '))
+    numberOfGenerations = int(input('Enter Number of Generations: '))
+    patriarch, matriarch, population = setUpWorld(initialPopulationSize) # save the details of the patriarch, matriach, and the initial population
 
-    theNextGeneration = myPopulation.return_member_list();
-    for x in range(0,len(theNextGeneration)): # go through every member of the population and find them a random hookup
-        for j in range(0,2): # give everyone a couple chances per generation to breed
-            myPopulation.mate(theNextGeneration[x],random.choice(theNextGeneration))
-        theNextGeneration[x].get_older() # reduce everyone's life counter
-    myPopulation.clean_up() # remove non-virile people from the breeding pool
-    #myPopulation.print_members()
+    
+    for i in range(0,numberOfGenerations):
+        population = processGeneration(population)
 
-myPopulation.print_members()
-print (a.name,": ", a.genome)
-print (b.name,": ", b.genome)
+
+    #population.print_members()
+    #print ("population size is: ",population.population_size())
+    print (patriarch.name,"genome: ",patriarch.genome)
+    print (matriarch.name,"genome: ",matriarch.genome)
+
+
+if __name__ == "__main__":
+    main()
